db2e21b82b255f5ddf642897850804147dc6daa3
SkyCrescent <crescentnounga@gmail.com>
2024-07-23 18:49:24 +0100

    modif2

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
    trailingSlash: true, // Ajoute un slash à la fin de chaque URL
   // output: 'export', // Utiliser le nouvel output pour les exports statiques
    //distDir: 'sort', // Le dossier de sortie de build, changez 'build' par le dossier de votre choix
-//   distDir: 'build', // Le dossier de sortie de build, changez 'build' par le dossier de votre choix
+  distDir: 'build', // Le dossier de sortie de build, changez 'build' par le dossier de votre choix
 
   // outdir: './out', // Le dossier de sortie pour les fichiers HTML statiques
 };
