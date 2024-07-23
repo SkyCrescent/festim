206d8638e2d39141ccf5ff9068d628d7e212209f
SkyCrescent <crescentnounga@gmail.com>
2024-07-23 19:02:15 +0100

    modif3

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   trailingSlash: true, // Ajoute un slash à la fin de chaque URL
-  // output: 'export', // Utiliser le nouvel output pour les exports statiques
+   // trailingSlash: true, // Ajoute un slash à la fin de chaque URL
+  output: 'export', // Utiliser le nouvel output pour les exports statiques
    //distDir: 'sort', // Le dossier de sortie de build, changez 'build' par le dossier de votre choix
-  distDir: 'build', // Le dossier de sortie de build, changez 'build' par le dossier de votre choix
+  //distDir: 'build', // Le dossier de sortie de build, changez 'build' par le dossier de votre choix
 
   // outdir: './out', // Le dossier de sortie pour les fichiers HTML statiques
 };
