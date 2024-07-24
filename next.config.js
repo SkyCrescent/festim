1d4b807c15c316dc98f20fdab7ca1b32ba885fac
SkyCrescent <crescentnounga@gmail.com>
2024-07-24 11:39:01 +0100

    ajput du mouvement des images2

--- next.config.js
+++ next.config.js
@@ -5,9 +5,7 @@ const nextConfig = {
   // trailingSlash: true, // Ajoute un slash à la fin de chaque URL
    //distDir: 'sort', // Le dossier de sortie de build, changez 'build' par le dossier de votre choix
   //distDir: 'build', // Le dossier de sortie de build, changez 'build' par le dossier de votre choix
-//pour builder sur vercel fo retirer cette partie && next export dans package
-  //vercel il ya que output ainsi que les hebergeur telle que hostinguer
-  // outdir: './out', // Le dossier de sortie pour les fichiers HTML statiques
+ // outdir: './out', // Le dossier de sortie pour les fichiers HTML statiques
 };
 
 module.exports = nextConfig;
